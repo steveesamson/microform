b38f96aa73eb388ce0c047ec00c428e3da478e87
Steve S. Samson <stevee.samson@gmail.com>
2024-01-04 23:16:52 +0100

    move style to static

--- docs/_app/immutable/chunks/stores.VzxRkInt.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.8Zw2_zZi.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
